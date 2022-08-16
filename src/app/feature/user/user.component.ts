8315a32b3f31675e3198930f73456db668ad9ace
thuanduy <thuanduy73h1@gmail.com>
2022-08-16 22:44:18 +0700

    update code

--- src/app/feature/user/user.component.ts
+++ src/app/feature/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  modalRef!: BsModalRef;
+  formPickRoom: FormGroup;
+  constructor(
+    public modalService: BsModalService,
+    private fb: FormBuilder
+  ) {
+    this.formPickRoom = this.fb.group({
+      
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  showChat(): void {
+    console.log("OK");
+  }
+
+  showModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-dialog-centered' });
+  }
+
+  pickRoom() {
+    console.log('run');
+  }
+  
 
 }
