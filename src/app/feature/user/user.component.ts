6b0d01ac3c29497fd51ccb283f5a40b08b180af9
thuanduy <thuanduy73h1@gmail.com>
2022-08-19 00:12:16 +0700

    update code

--- src/app/feature/user/user.component.ts
+++ src/app/feature/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import * as dayjs from 'dayjs'
 
 @Component({
   selector: 'app-user',
@@ -10,12 +11,16 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 export class UserComponent implements OnInit {
   modalRef!: BsModalRef;
   formPickRoom: FormGroup;
+  dateNow: string = dayjs().format("HH:mm DD/MM/YYYY");
+
   constructor(
     public modalService: BsModalService,
     private fb: FormBuilder
   ) {
     this.formPickRoom = this.fb.group({
-
+      dateCheckIn: ['', [Validators.required]],
+      timeCheckIn: ['', [Validators.required]],
+      timeCheckOut: ['', [Validators.required]]
     });
   }
 
@@ -32,6 +37,14 @@ export class UserComponent implements OnInit {
 
   pickRoom() {
     console.log('run');
+  }
+
+  nexStept() {
+
+  }
+
+  hideModal(): void {
+    this.modalRef.hide();
   }
 
 
